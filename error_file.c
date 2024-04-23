8b734f34d5ed3e4dd9e8ebcb5656d42f0cbd6458
Hassan220022 <hassansonson2002@gmail.com>
2024-04-23 17:47:36 +0200

    bor3i

--- error_file.c
+++ error_file.c
@@ -1,13 +1,16 @@
 #include "monty.h"
 
 /**
- * error - Prints error mesages according to the error code
- * @errorCode: the error code
- * (1) => no given file or more than one file to the program
- * (2) => can't open file
- * (3) => file provied have invalid instruction
- * (4) => program cann't allocat more memory
- * (5) => invalid argument || invalid data type to be pushed
+ * error - Prints appropiate error messages determined by their error code.
+ * @errorCode: The error codes are the following:
+ * (1) => The user does not give any file or more than one file to the program.
+ * (2) => The file provided is not a file that can be opened or read.
+ * (3) => The file provided contains an invalid instruction.
+ * (4) => When the program is unable to malloc more memory.
+ * (5) => When the parameter passed to the instruction "push" is not an int.
+ * (6) => When the stack it empty for pint.
+ * (7) => When the stack it empty for pop.
+ * (8) => When stack is too short for operation.
  */
 void error(int errorCode, ...)
 {
@@ -23,70 +26,74 @@ void error(int errorCode, ...)
 		fprintf(stderr, "USAGE: monty file\n");
 		break;
 	case 2:
-		file_name = va_arg(args, char *);
-		fprintf(stderr, "Error: Can't open file %s\n", file_name);
+		fprintf(stderr, "Error: Can't open file %s\n",
+				va_arg(args, char *));
 		break;
 	case 3:
 		line_number = va_arg(args, int);
-		fprintf(stderr, "L%d: unknown instruction\n", line_number);
+		file_name = va_arg(args, char *);
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, file_name);
 		break;
 	case 4:
 		fprintf(stderr, "Error: malloc failed\n");
 		break;
 	case 5:
-		line_number = va_arg(args, int);
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", va_arg(args, int));
 		break;
 	default:
-		fprintf(stderr, "Unknown error\n");
+		break;
 	}
-
-	va_end(args);
+	free_nodes();
 	exit(EXIT_FAILURE);
 }
 
 /**
  * error_more - Prints error mesages according to the error code
  * @errorCode: the error code
- * @line_number: the line number
- * (6) => can't print an empty stack
- * (7) => can't pop an empty stack
- * (8) => can't swap, short stack
- * (9) => can't divide by zero
- * (10) => The number inside a node is outside ASCII bounds.
- * (11) => The stack is empty.
+ * (6) => When the stack it empty for pint.
+ * (7) => When the stack it empty for pop.
+ * (8) => When stack is too short for operation.
+ * (9) => Division by zero.
+ * (10) ~> The number inside a node is outside ASCII bounds.
+ * (11) ~> The stack is empty.
  */
-void error_more(int errorCode, int line_number, ...)
+void error_more(int errorCode, ...)
 {
-	if (line_number < 0)
-	{
-		fprintf(stderr, "Error: Line number is invalid\n");
-		exit(EXIT_FAILURE);
-	}
+	va_list args;
+	char *file_name;
+	int line_number;
 
+	va_start(args, errorCode);
 	switch (errorCode)
 	{
 	case 6:
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n",
+				va_arg(args, int));
 		break;
 	case 7:
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n",
+				va_arg(args, int));
 		break;
 	case 8:
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		line_number = va_arg(args, unsigned int);
+		file_name = va_arg(args, char *);
+		fprintf(stderr, "L%d: can't %s, stack too short\n", line_number, file_name);
 		break;
 	case 9:
-		fprintf(stderr, "L%d: division by zero\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n",
+				va_arg(args, unsigned int));
 		break;
 	case 10:
-		fprintf(stderr, "L%d: value out of ASCII bounds\n", line_number);
+		line_number = va_arg(args, int);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		break;
 	case 11:
-		fprintf(stderr, "L%d: stack empty\n", line_number);
+		line_number = va_arg(args, int);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		break;
 	default:
-		fprintf(stderr, "Unknown error\n");
+		break;
 	}
-
+	free_nodes();
 	exit(EXIT_FAILURE);
 }
